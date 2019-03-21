e35a9981135f372cc4ece94a17e45a580d326fa0
AmrHussienM <amr.hussien97@outlook.com>
2019-03-21 13:51:04 +0200

    bb

---
+++ app/src/main/java/com/example/letsgo/LogIn/LogInActivity.java
@@ -0,0 +1,15 @@
+package com.example.letsgo.LogIn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.letsgo.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_);
+    }
+}
---
+++ app/src/main/java/com/example/letsgo/Registeration/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.letsgo.Registeration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.letsgo.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
