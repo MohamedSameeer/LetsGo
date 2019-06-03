8c3d08671d01c094a6d0da2329fb8edddc94a7be
Mohamed Sameer <mohamedsameeer96@gmail.com>
2019-06-03 06:11:36 +0200

    fixed drawer

--- app/src/main/java/com/example/letsgo/DrawerMainActivity.java
+++ app/src/main/java/com/example/letsgo/DrawerMainActivity.java
@@ -1,6 +1,9 @@
 package com.example.letsgo;
 
+import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
+import android.support.design.internal.NavigationMenu;
+import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
@@ -9,23 +12,34 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 
 import com.example.letsgo.Country.CountryActivity;
+import com.example.letsgo.Favorite.Favorite;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DrawerMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Fragment fragment;
-
+    private final static Fragment fCountry=new CountryActivity(),
+            fFavorite=new Favorite();
+    FirebaseAuth mAuth;
+    int fragmentType;
+    MenuItem menuItem;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        fragment = fragment = new CountryActivity();
+
+        mAuth=FirebaseAuth.getInstance();
+        fragment = fCountry;
+
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -33,21 +47,54 @@ public class DrawerMainActivity extends AppCompatActivity implements NavigationV
         navigationView.setNavigationItemSelectedListener(this);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 fragment).commit();
+        navigationView.getMenu().getItem(0).setChecked(true);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        this.menuItem=menuItem;
+
         int id = menuItem.getItemId();
 
-        if (id == R.id.nav_home) {
-            fragment = new CountryActivity();
-        } else if (id == R.id.nav_categroies) {
-
+        switch (id)
+        {
+            case R.id.nav_home:
+                fragment=fCountry;
+                fragmentType=0;
+                break;
+            case R.id.nav_save:
+                fragment=fFavorite;
+                fragmentType=1;
+                break;
+            case R.id.nav_logOut:
+                fragmentType=2;
+                mAuth.signOut();
         }
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 fragment).commit();
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+            if(fragmentType==1) {
+                fragment = fCountry;
+                navigationView.getMenu().getItem(0).setChecked(true);
+                fragmentType=0;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        fragment).commit();
+
+            }else if(fragmentType==0){
+                finish();
+            }
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
--- app/src/main/java/com/example/letsgo/Favorite/Favorite.java
+++ app/src/main/java/com/example/letsgo/Favorite/Favorite.java
@@ -1,43 +1,51 @@
 package com.example.letsgo.Favorite;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.letsgo.Place.PlaceActivity;
 import com.example.letsgo.R;
 
-public class Favorite extends AppCompatActivity {
+public class Favorite extends Fragment {
 
+    View view;
     FavoriteAdapter placesAdapter;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorite);
+        view = inflater.inflate(R.layout.activity_favorite, container, false);
 
-        RecyclerView recyclerView=findViewById(R.id.container_favorite_places);
-      LinearLayoutManager  linearLayoutManager=new LinearLayoutManager(this);
+        RecyclerView recyclerView = view.findViewById(R.id.container_favorite_places);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
 
-        final PlaceActivity placeActivity=new PlaceActivity();
-        FavoritePresenter favoritePresenter=new FavoritePresenter(recyclerView,linearLayoutManager);
-        placesAdapter =favoritePresenter.getPlacesAdapter();
+        final PlaceActivity placeActivity = new PlaceActivity();
+        FavoritePresenter favoritePresenter = new FavoritePresenter(recyclerView, linearLayoutManager);
+        placesAdapter = favoritePresenter.getPlacesAdapter();
         favoritePresenter.getData();
         placesAdapter.setOnItemClickListener(new FavoriteAdapter.OnItemClickListener() {
             @Override
             public void onClick(int position) {
-                Intent i=new Intent(Favorite.this, PlaceActivity.class);
-                i.putExtra("img",""+ placesAdapter.getList().get(position).getImg());
-                i.putExtra("name",""+ placesAdapter.getList().get(position).getName());
-                i.putExtra("desc",""+ placesAdapter.getList().get(position).getDescription());
-                i.putExtra("address",""+ placesAdapter.getList().get(position).getAddress());
-                i.putExtra("city",""+ placesAdapter.getList().get(position).getCity());
-                i.putExtra("category",""+ placesAdapter.getList().get(position).getCategory());
-                i.putExtra("price",""+ placesAdapter.getList().get(position).getPrice());
-                i.putExtra("from",""+ placesAdapter.getList().get(position).getDurationFrom());
-                i.putExtra("to",""+ placesAdapter.getList().get(position).getDurationTo());
-                i.putExtra("fromClass","favorite");/*
+                Intent i = new Intent(view.getContext(), PlaceActivity.class);
+                i.putExtra("img", "" + placesAdapter.getList().get(position).getImg());
+                i.putExtra("name", "" + placesAdapter.getList().get(position).getName());
+                i.putExtra("desc", "" + placesAdapter.getList().get(position).getDescription());
+                i.putExtra("address", "" + placesAdapter.getList().get(position).getAddress());
+                i.putExtra("city", "" + placesAdapter.getList().get(position).getCity());
+                i.putExtra("category", "" + placesAdapter.getList().get(position).getCategory());
+                i.putExtra("price", "" + placesAdapter.getList().get(position).getPrice());
+                i.putExtra("from", "" + placesAdapter.getList().get(position).getDurationFrom());
+                i.putExtra("to", "" + placesAdapter.getList().get(position).getDurationTo());
+                i.putExtra("fromClass", "favorite");/*
                 placeActivity.placeDetails(""+placesAdapter.getList().get(position).getName(),
                         ""+  placesAdapter.getList().get(position).getDescription(),
                         placesAdapter.getList().get(position).getImg()+"",
@@ -52,5 +60,6 @@ public class Favorite extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        return view;
     }
 }
